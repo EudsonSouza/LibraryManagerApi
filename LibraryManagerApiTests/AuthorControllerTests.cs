eb39268aa9f21d3125a44f00171079cd79dd7446
Eudson Souza <T0261224@vli-logistica.com.br>
2024-07-31 18:13:14 -0300

    Add `AuthorControllerTests.cs` and test project configurations

---
+++ LibraryManagerApiTests/AuthorControllerTests.cs
@@ -0,0 +1,69 @@
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Infrastructure.Data;
+using Domain.Models;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+public class AuthorsControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory<Program> _factory;
+
+    public AuthorsControllerTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAllAuthors_ReturnsOkResponse()
+    {
+        // Arrange
+        await EnsureAuthorCreatedAsync(new Author { Name = "Initial Author" });
+        var request = new HttpRequestMessage(HttpMethod.Get, "author");
+
+        // Act
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        var authors = JsonConvert.DeserializeObject<List<Author>>(responseString);
+        Assert.NotNull(authors);
+        Assert.NotEmpty(authors); // Adicionado para garantir que a lista não está vazia
+    }
+
+    [Fact]
+    public async Task CreateAuthor_ReturnsCreatedResponse()
+    {
+        // Arrange
+        var author = new Author { Name = "New Author" };
+        var content = new StringContent(JsonConvert.SerializeObject(author), Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _client.PostAsync("author", content);
+
+        // Assert
+        
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+
+        // Verify
+        var responseString = await response.Content.ReadAsStringAsync();
+        var createdAuthor = JsonConvert.DeserializeObject<Author>(responseString);
+        Assert.NotNull(createdAuthor);
+        Assert.Equal("New Author", createdAuthor.Name);
+    }
+
+    private async Task EnsureAuthorCreatedAsync(Author author)
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(author), Encoding.UTF8, "application/json");
+        var response = await _client.PostAsync("/author", content);
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ LibraryManagerApiTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure.Data;
+using System.Linq;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Remover o contexto do SqlContext registrado
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<SqlContext>));
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Adicionar o contexto do SqlContext usando InMemoryDatabase
+            services.AddDbContext<SqlContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+
+            // Criar o escopo para inicializar o banco de dados
+            var serviceProvider = services.BuildServiceProvider();
+            using var scope = serviceProvider.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<SqlContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+}
